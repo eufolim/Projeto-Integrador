ce5bc75ec44666eb977e065678a511d1f8bb489b
Miguel Rossi Fermo <miguelrossifermo05@gmail.com>
2024-08-28 10:59:23 -0300

    ja vou atualizar novamente

---
+++ codigos/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, render_template_string, request, redirect, url_for, session
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+# Aqui são colocadas as rotas para cada página html
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('/index.html')
+
+@app.route('/sala', methods=['POST'])
+def sum():
+    return 1 + 1
+
+if __name__ == '__main__':
+    app.run(debug=True)
